bc5e965976adea154e9ac4f753fb2831f2ee0acb
mnrdsnts <meinardsantoss@gmail.com>
2024-10-17 15:55:17 +0800

    added authentication; login and logout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,109 +1,38 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import './App.css'
-import axios from 'axios'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import Home from './components/Home'
+import { Auth0Provider, useAuth0 } from "@auth0/auth0-react";
+
+function LoginButton() {
+  const { loginWithRedirect } = useAuth0();
+  return <button className='text-white font-bold p-2 rounded-xl' style={{backgroundColor: '#ff8906'}} onClick={() => loginWithRedirect()}>Log In</button>;
+}
+
+function LogoutButton() {
+  const { logout } = useAuth0();
+  return (
+    <button className='text-white font-bold p-2 rounded-xl' style={{backgroundColor: '#ff8906'}} onClick={() => logout({ returnTo: window.location.origin })}>
+      Log Out
+    </button>
+  );
+}
 
 const App = () => {
-  const [input, setInput] = useState('');
-  const [content, setContent] = useState([]);
-  const [edit, setEdit] = useState(null);
-  const [editInput, setEditInput] = useState('');
-
-  const handleSubmit = async () => {
-    try {
-      const res = await axios.post('http://localhost:3000/addNew', { input });
-      fetchData();
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  const fetchData = async () => {
-    const res = await axios.get('http://localhost:3000/contents');
-    setContent(res.data);
-    // console.log(res.data);
-  }
-
-  useEffect(() => {
-    fetchData();
-  }, [content]); // making content as a dependency triggers re-render after every content makes changes
-
-  const handleDelete = async (id) => {
-    try {
-      const res = await axios.post('http://localhost:3000/removeItem', { id });
-      fetchData();
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  const handleEdit = async (id, currInput) => {
-    setEdit(id);
-    setEditInput(currInput);
-  }
-
-  const handleEdited = async (id) => {
-    try {
-      const res = await axios.post('http://localhost:3000/updateItem', { id, editInput }); 
-      setEdit(null);
-      fetchData();
-  } catch (error) {
-      console.error(error);
-  }
-  }
-
   return (
-    <div className='flex m-10 flex-col'>
-      <header className='flex justify-between'>
+    <div>
+      <Auth0Provider
+        domain="dev-li6a212imhcsv22q.us.auth0.com"
+        clientId="pU2NTaG1rp7DCd8XDqecCbke8G9hW0ZP"
+        authorizationParams={{ redirect_uri: window.location.origin }}
+      >
         <div>
-          <input 
-            value={input} 
-            onChange={(e) => setInput(e.target.value)} // fetching the input data for passing to the api
-            className='border text-white border-white p-2' type="text" placeholder='Add New' />
-          <button 
-            onClick={handleSubmit}
-            className='font-bold p-2 rounded-se-full bg-white m-2'>+
-          </button>
-        </div>
-        <h1 className='text-white mx-4'>Pebble Developers</h1>
-      </header>
-
-      <section className='flex flex-wrap my-20'>
-        {content.map((item) => ( // mapping every content, using index as key for formality and grammar
-          <div key={item._id} className='flex gap-5 p-4 m-2 rounded-full' style={{backgroundColor: '#F25F4C'}}>
-            {/* <h1 
-                  onClick={() => handleEdit(item._id, item.input)}
-                  onInput={(e) => setEditInput(e.target.textContent)}
-                  contentEditable={edit === item._id}
-                  onBlur={() => handleEdited(item._id)}
-                  suppressContentEditableWarning={true}
-                  className='text-white font-bold' style={{backgroundColor: '#F25F4C'}}>
-                  {edit === item._id ? editInput : item.input}
-                </h1> */}
-              {edit === item._id ? (
-                <input
-                  value={editInput}
-                  onChange={(e) => setEditInput(e.target.value)}
-                  onBlur={() => handleEdited(item._id)}
-                  className='w-auto text-white font-bold'
-                  style={{ backgroundColor: '#F25F4C' }}
-                />
-              ) : (
-                <div 
-                  onClick={() => handleEdit(item._id, item.input)}
-                  className='text-white font-bold' style={{ backgroundColor: '#F25F4C' }}>
-                  {item.input}
-                </div>
-              )}
-            <div className='flex'>
-              <FontAwesomeIcon className='p-1' style={{backgroundColor: '#F25F4C'}} onClick={() => handleDelete(item._id)} icon={faTrash} />
-            </div>
+          <div className='flex justify-end gap-5 m-10'>
+            <LoginButton />
+            <LogoutButton />
           </div>
-        ))
-        }
-      </section>
+          <Home />
+        </div>
+      </Auth0Provider>
     </div>
   )
 }
